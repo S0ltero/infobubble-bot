2e801531112ea0502e1d0c0cc0148817e8503b49
S0ltero <AlexsSobko61@gmail.com>
2022-01-06 13:21:47 +0300

    Add `/api/user` endpoint - Change view for `api/users` endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import *
 
 app_name = 'api'
 
 urlpatterns = [
-    path('users/', UserView.as_view()),
+    re_path(r'^user/', UserView.as_view()),
+    path('users/', UserListView.as_view()),
     path('channels/', ChannelView.as_view()),
     path('rate/', UserRateView.as_view())
 ]
