995bf3a668efaaff3c84cdf04a59d205a48c5040
S0ltero <AlexsSobko61@gmail.com>
2022-01-07 16:50:36 +0300

    Add additional url endpoint with get parameter `user_id`

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,8 @@ from .views import *
 app_name = 'api'
 
 urlpatterns = [
-    re_path(r'^user/', UserView.as_view()),
+    path('user/', UserView.as_view()),
+    path('user/<user_id>', UserView.as_view()),
     path('users/', UserListView.as_view()),
     path('channels/', ChannelView.as_view()),
     path('rate/', UserRateView.as_view())
