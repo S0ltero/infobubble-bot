a2f698f6630d9443e7c1b7458a674af0ca2c4488
S0ltero <AlexsSobko61@gmail.com>
2022-01-08 16:10:03 +0300

    Add api endpoints for creating and getting history

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('user/<user_id>', UserView.as_view()),
     path('users/', UserListView.as_view()),
     path('channels/', ChannelView.as_view()),
-    path('rate/', UserRateView.as_view())
+    path('rate/', UserRateView.as_view()),
+    path('history/', HistoryMessageView.as_view()),
+    path('history/<user_id>/<channel_id>/<message_id>', HistoryMessageView.as_view())
 ]
