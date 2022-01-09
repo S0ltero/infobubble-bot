7ddb948d0ccc1c5ee545ff417b84d9d8127018ed
S0ltero <AlexsSobko61@gmail.com>
2022-01-09 13:23:48 +0300

    Add `get_channel_ids` method to `TelegramUserSerializer`

--- telegram/serializers.py
+++ telegram/serializers.py
@@ -2,16 +2,22 @@ from rest_framework import serializers
 
 from .models import (
     TelegramUser,
+    TelegramChannel,
     HistoryMessage,
     UserMessageRate
 )
 
 
 class TelegramUserSerializer(serializers.ModelSerializer):
+    channel_ids = serializers.SerializerMethodField()
 
     class Meta:
         model = TelegramUser
         fields = "__all__"
+
+    def get_channel_ids(self, obj):
+        channel_ids = TelegramChannel.objects.filter(tags__overlap=obj.filters).values_list("id", flat=True)
+        return channel_ids
 
 
 class HistoryMessageSerializer(serializers.ModelSerializer):
