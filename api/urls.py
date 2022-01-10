89d35d7b2d2b26fc0a8266212ed006690068c74a
S0ltero <AlexsSobko61@gmail.com>
2022-01-10 15:45:03 +0300

    Update `urls.py`

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path('user/', UserView.as_view()),
     path('user/<user_id>', UserView.as_view()),
     path('users/', UserListView.as_view()),
-    path('channels/', ChannelView.as_view()),
+    path('channel/', ChannelView.as_view()),
+    path('channel/<channel_id>', ChannelView.as_view()),
+    path('channels/', ChannelsView.as_view()),
     path('rate/', UserRateView.as_view()),
+    path('subscribe/', UserSubscribeView.as_view()),
     path('history/', HistoryMessageView.as_view()),
     path('history/<user_id>/<channel_id>/<message_id>', HistoryMessageView.as_view())
 ]
