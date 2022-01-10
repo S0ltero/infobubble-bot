889853ad158936bf65c51d5d494fdd766414b275
S0ltero <AlexsSobko61@gmail.com>
2022-01-10 16:46:09 +0300

    Register `UserSubscribe` and `HistoryMessage` models in admin

--- telegram/admin.py
+++ telegram/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import TelegramUser, TelegramChannel, UserMessageRate
+from .models import TelegramUser, TelegramChannel, UserMessageRate, UserSubscribe, HistoryMessage
 
 from import_export import resources
 from import_export.admin import ImportExportActionModelAdmin
 
 admin.site.register(TelegramUser)
 admin.site.register(UserMessageRate)
+admin.site.register(UserSubscribe)
+admin.site.register(HistoryMessage)
 
 class TelegramChannelResource(resources.ModelResource):
 
