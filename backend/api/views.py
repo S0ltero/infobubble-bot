105849d8214583d04315c05264cd39b554b2d0c7
S0ltero <AlexsSobko61@gmail.com>
2022-08-11 10:12:07 +0300

    Remove unused import

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
 
 from telegram.models import TelegramChannel, TelegramMessage, TelegramUser, HistoryMessage, UserSubscribe, Config
 from telegram.serializers import (
